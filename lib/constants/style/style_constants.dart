bff250172e07751eb7e131a901f3736d3fa010c0
Ralf <135024553+rtamb0@users.noreply.github.com>
2025-12-18 16:37:43 +0700

    Change app colour to orange

--- lib/constants/style/style_constants.dart
+++ lib/constants/style/style_constants.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class StyleConstants {
   // Filled with example constants
   static const Color scaffoldBackgroundColor = Colors.white;
-  static const Color colorOfApp = Color.fromRGBO(12, 98, 235, 1);
+  static const Color colorOfApp = Color.fromRGBO(244, 124, 32, 1);
   static const Color colorTitle = Colors.black;
 }
